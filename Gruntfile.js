331fd2cefec4675e093e9914f34f20225effcf7e
jsanahuja <bannss1@gmail.com>
2019-07-12 22:31:10 +0200

    Travis CI tests

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        qunit: {
+            files: ['test/index.html']
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.registerTask('test', 'qunit');
+};
