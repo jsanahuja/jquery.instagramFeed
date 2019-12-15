51a25dbb7bce761cf21f1b16e9860835ee155997
jsanahuja <bannss1@gmail.com>
2019-12-15 18:25:27 +0100

    Script to run builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,30 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        'node-minify': {
+            gcc: {
+                files: {
+                    'jquery.instagramFeed.min.js': ['jquery.instagramFeed.js']
+                }
+            }
+        },
         qunit: {
-            files: ['test.html']
+            files: ['test.html'],
+			options: {
+				puppeteer: {
+					ignoreDefaultArgs: true,
+					args: [
+						"--headless",
+						"--disable-web-security",
+						"--allow-file-access-from-files"
+					]
+				},
+				timeout: 10000
+			},
         }
     });
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.registerTask('test', 'qunit');
+
+    grunt.loadNpmTasks('grunt-node-minify');
+    grunt.registerTask('dist', 'node-minify');
 };
