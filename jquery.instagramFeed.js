3f076631b672427b368417fe2a6d2c2d4456ff55
jsanahuja <bannss1@gmail.com>
2020-01-07 21:41:29 +0100

    Format correction

--- jquery.instagramFeed.js
+++ jquery.instagramFeed.js
@@ -66,7 +66,7 @@
             if(options.get_data){
                 options.callback(data);
                 return;
-            }
+            }   
                 
             //Setting styles
             var styles = {
@@ -88,17 +88,17 @@
             var html = "";
             //Displaying profile
             if(options.display_profile){
-                html +=     "<div class='instagram_profile'" +styles.profile_container +">";
-                html +=     "    <img class='instagram_profile_image' src='"+ data.profile_pic_url +"' alt='"+ (is_tag ? data.name + " tag pic" : data.username + " profile pic") +"'"+ styles.profile_image +" />";
+                html += "<div class='instagram_profile'" +styles.profile_container +">";
+                html += "<img class='instagram_profile_image' src='"+ data.profile_pic_url +"' alt='"+ (is_tag ? data.name + " tag pic" : data.username + " profile pic") +"'"+ styles.profile_image +" />";
                 if(is_tag)
-                    html += "    <p class='instagram_tag'"+ styles.profile_name +"><a href='https://www.instagram.com/explore/tags/"+ options.tag +"' rel='noopener' target='_blank'>#"+ options.tag +"</a></p>";
+                    html += "<p class='instagram_tag'"+ styles.profile_name +"><a href='https://www.instagram.com/explore/tags/"+ options.tag +"' rel='noopener' target='_blank'>#"+ options.tag +"</a></p>";
                 else
-                    html += "    <p class='instagram_username'"+ styles.profile_name +">@"+ data.full_name +" (<a href='https://www.instagram.com/"+ options.username +"' rel='noopener' target='_blank'>@"+options.username+"</a>)</p>";
+                    html += "<p class='instagram_username'"+ styles.profile_name +">@"+ data.full_name +" (<a href='https://www.instagram.com/"+ options.username +"' rel='noopener' target='_blank'>@"+options.username+"</a>)</p>";
         
                 if(!is_tag && options.display_biography)
-                    html += "    <p class='instagram_biography'"+ styles.profile_biography +">"+ data.biography +"</p>";
+                    html += "<p class='instagram_biography'"+ styles.profile_biography +">"+ data.biography +"</p>";
         
-                html +=     "</div>";
+                html += "</div>";
             }
 
             //image size
@@ -111,7 +111,7 @@
                     var imgs = (data.edge_owner_to_timeline_media || data.edge_hashtag_to_media).edges;
                         max = (imgs.length > options.items) ? options.items : imgs.length;
                     
-                    html +=         "<div class='instagram_gallery'>";
+                    html += "<div class='instagram_gallery'>";
                     for(var i = 0; i < max; i++){
                         var url = "https://www.instagram.com/p/" + imgs[i].node.shortcode,
                             image, type_resource, caption;
@@ -129,20 +129,20 @@
                                 type_resource = "image";
                                 image = imgs[i].node.thumbnail_resources[image_index].src;
                         }
-						
-						if(typeof imgs[i].node.edge_media_to_caption.edges[0] !== "undefined"){
-							caption = imgs[i].node.edge_media_to_caption.edges[0].node.text;
-						}else if(typeof imgs[i].node.accessibility_caption !== "undefined"){
-							caption = imgs[i].node.accessibility_caption;
-						}else{
-							caption = (is_tag ? data.name : data.username) + " image " + i;
-						}
+                        
+                        if(typeof imgs[i].node.edge_media_to_caption.edges[0] !== "undefined"){
+                            caption = imgs[i].node.edge_media_to_caption.edges[0].node.text;
+                        }else if(typeof imgs[i].node.accessibility_caption !== "undefined"){
+                            caption = imgs[i].node.accessibility_caption;
+                        }else{
+                            caption = (is_tag ? data.name : data.username) + " image " + i;
+                        }
 
-                        html +=     "    <a href='" + url +"' class='instagram-" + type_resource + "' rel='noopener' target='_blank'>";
-                        html +=     "       <img src='" + image + "' alt='" + caption + "'" + styles.gallery_image +" />";
-                        html +=     "    </a>";
+                        html += "<a href='" + url +"' class='instagram-" + type_resource + "' rel='noopener' target='_blank'>";
+                        html += "<img src='" + image + "' alt='" + caption + "'" + styles.gallery_image +" />";
+                        html += "</a>";
                     }
-                    html +=         "</div>";
+                    html += "</div>";
                 }
             }
             
@@ -150,13 +150,13 @@
                 var igtv = data.edge_felix_video_timeline.edges,
                     max = (igtv.length > options.items) ? options.items : igtv.length
                 if(igtv.length > 0){
-                    html +=     "<div class='instagram_igtv'>";
+                    html += "<div class='instagram_igtv'>";
                     for(var i = 0; i < max; i++){
-                        html += "    <a href='https://www.instagram.com/p/"+ igtv[i].node.shortcode +"' rel='noopener' target='_blank'>";
-                        html += "        <img src='"+ igtv[i].node.thumbnail_src +"' alt='"+ options.username +" instagram image "+ i+"'"+styles.gallery_image+" />";
-                        html += "    </a>";
+                        html += "<a href='https://www.instagram.com/p/"+ igtv[i].node.shortcode +"' rel='noopener' target='_blank'>";
+                        html += "<img src='"+ igtv[i].node.thumbnail_src +"' alt='"+ options.username +" instagram image "+ i+"'"+styles.gallery_image+" />";
+                        html += "</a>";
                     }
-                    html +=     "</div>";
+                    html += "</div>";
                 }
             }
             $(options.container).html(html);
